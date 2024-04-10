a5d4abfe4c37378faca00727e0d0b23a6083494d
PromitDey <promitdey140@gmail.com>
2024-04-10 12:53:21 +0530

    10_4_24

---
+++ Final_IPL/Frontend/src/utils/teamMembers.js
@@ -0,0 +1,9 @@
+export const tm1 =
+  "https://media.licdn.com/dms/image/C5603AQG83bLKq8IxeA/profile-displayphoto-shrink_400_400/0/1656422373441?e=1718236800&v=beta&t=mLFn9A2jid_sOlnqj2yN8Qlg6JJ2f5PwbtPxltGWCdw";
+
+export const tm2 = "https://media.licdn.com/dms/image/D4D03AQG1D6tpq8tP1w/profile-displayphoto-shrink_400_400/0/1695538114040?e=1718236800&v=beta&t=oJn5G2uG_vt_ezbLlFKovSbWpxfrD614oOT2z1RL588";
+
+export const tm3 =
+  "https://media.licdn.com/dms/image/D5603AQG8-ekjSQJT9g/profile-displayphoto-shrink_400_400/0/1704030313422?e=1718236800&v=beta&t=vu8A4cDRFaBrwymJG5W6fVNXbAmFRUr3_7VGh2jQuoI";
+
+export const tm4 = "";
